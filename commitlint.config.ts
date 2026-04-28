398d99bf2bbcad9351a043f859e5083919b831ae
Budi Syahiddin <me@inve.rs>
2026-04-29 04:43:58 +0800

    cicd: hardpin release.yml

---
+++ commitlint.config.ts
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "chore",
+        "style",
+        "refactor",
+        "ci",
+        "test",
+        "revert",
+        "perf",
+        "report",
+        "cicd",
+        "security",
+      ],
+    ],
+  },
+};
